0266d5bc3cd12d4ae57b9147cd6472a15fe7c281
lifd <lfd547@outlook.com>
2024-10-31 23:43:18 +0800

    MD: 创建网格

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexMetrics : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public static class HexMetrics {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public const float outerRadius = 10f;
+
+	public const float innerRadius = outerRadius * 0.866025404f;
 }
